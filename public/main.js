00ec8c0594ca30f3e2ad986ccd19fe2d77212df6
Chris Snyder <bumishness@gmail.com>
2015-02-10 20:16:00 -0600

    Various junk

--- public/main.js
+++ public/main.js
@@ -9,43 +9,47 @@ var normalizeDimensions = function(w, h, canvases) {
   });
 }
 
-var startVideoCapture = function(video) {
+var startVideoCapture = function(video, delay) {
   var earthCanvas = document.querySelector('#earth-canvas'),
       earthContext = earthCanvas.getContext('2d'),
 
-      moonCanvas = document.querySelector('#moon-canvas'),
-      moonContext = moonCanvas.getContext('2d'),
-
-      neoOneCanvas = document.querySelector('#neo-one-canvas'),
-      neoOneContext = neoOneCanvas.getContext('2d'),
-
-      neoTwoCanvas = document.querySelector('#neo-two-canvas'),
-      neoTwoContext = neoTwoCanvas.getContext('2d');
+      spaceCanvas = document.querySelector('#space-canvas'),
+      spaceContext = spaceCanvas.getContext('2d');
 
   var currentStream = 0;
-
   var scale = 1;
   var imageData = [];
   var w = 640, h = 480;
   var fps = Math.floor(1000 / 30);
+  var delayInSeconds = delay / 1000;
 
-  normalizeDimensions(w, h, [earthCanvas, moonCanvas, neoOneCanvas, neoTwoCanvas]);
+  normalizeDimensions(w, h, [earthCanvas, spaceCanvas]);
 
   setInterval(function() {
     earthContext.drawImage(video, 0, 0, w, h);
     imageData.unshift(earthContext.getImageData(0, 0, w, h));
 
-    if ((imageData.length - 1) < fps * 2) return;
-    renderVideo(moonContext, imageData[(fps * 2)], w, h);
+    if ((imageData.length - 1) < Math.floor(fps * delayInSeconds)) return;
+    renderVideo(spaceContext, imageData[Math.floor(fps * delayInSeconds)], w, h);
 
     imageData = imageData.slice(0, fps * 15);
 
   }, Math.floor(fps));
 }
 
-var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || false;
+document.addEventListener('DOMContentLoaded', function() {
+  var delay = 2000;
 
-window.onload = function() {
+  location.search.slice(1).split('&').forEach(function (param) {
+    var pair = param.split('=');
+
+    if (pair[0] != 'delay') {
+      return;
+    }
+
+    delay = +pair[1];
+  });
+
   var hide = function(e) {
     this.style.display = 'none';
   }
@@ -57,32 +61,35 @@ window.onload = function() {
       goEl.classList.add('fade-out');
       videoEl.play();
 
-      startVideoCapture(videoEl);
+      startVideoCapture(videoEl, delay);
     });
   }
 
-  var video = document.querySelector('video'),
-      errBack = function(error) {
-        if (console) {
-          console.log("Video capture error: ", error.code);   
-        }
-      };
-  var videoObj = {video: true};
+  var video = document.querySelector('video');
+  var errBack = function (error) {
+    if (console) {
+      console.log("Video capture error: ", error);   
+    }
+  };
+
+  var videoObj = {
+    video: true
+  };
+
   if(navigator.getUserMedia) {
-    navigator.getUserMedia(videoObj, function(stream) {
+    navigator.getUserMedia(videoObj, function (stream) {
       video.src = stream;
       videoReceived(video);
     }, errBack);
   } else if(navigator.webkitGetUserMedia) {
-    navigator.webkitGetUserMedia(videoObj, function(stream) {
+    navigator.webkitGetUserMedia(videoObj, function (stream) {
       video.src = window.webkitURL.createObjectURL(stream);
       videoReceived(video);
     }, errBack);
-  }
-  else if(navigator.mozGetUserMedia) {
-    navigator.mozGetUserMedia(videoObj, function(stream) {
+  } else if(navigator.mozGetUserMedia) {
+    navigator.mozGetUserMedia(videoObj, function (stream) {
       video.src = window.URL.createObjectURL(stream);
       videoReceived(video);
     }, errBack);
   }
-}
+});
